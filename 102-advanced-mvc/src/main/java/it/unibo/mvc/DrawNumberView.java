f739e85c3a3fe32986f7b3575ba8861936e21fc0
Mattia Mularoni <mattia.mularoni@studio.unibo.it>
2024-11-27 11:09:56 +0100

    added displayError

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
@@ -24,4 +24,9 @@ public interface DrawNumberView {
      * @param res the result of the last draw
      */
     void result(DrawResult res);
+
+    /**
+     * This method display the error in the dialog. 
+     */
+    void displayError(String message);
 }
